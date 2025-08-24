16ff4a2345bc5975e6ca36621b7b703db9f7f31d
LaurenVictoriaFR <lauren.victoria@dcomp.ufs.br>
2025-08-24 19:13:40 -0300

    Update UserService.java

--- src/main/java/com/ratelikeamuse/service/UserService.java
+++ src/main/java/com/ratelikeamuse/service/UserService.java
@@ -2,6 +2,7 @@ package RateLikeAMuse.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import RateLikeAMuse.entity.Funcao;
 
 import RateLikeAMuse.entity.User;
 import RateLikeAMuse.repository.UserRepository;
@@ -17,14 +18,27 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
     //Esta função recebe um usuário e senha, após isso ela verifica se o usuário já existe, caso não exista ela criptografa a senha e cria o usuário.
-    public User registerUser(String username, String password) {
+    public User registerUser(String username, String password, String email) {
         if (userRepository.existsByUsername(username)) {
             throw new RuntimeException("Username já existente");
         }
-        User user = new User(username, passwordEncoder.encode(password));
+         if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Email já existente");
+        }
+        User user = new User(null, username, email, passwordEncoder.encode(password), Funcao.USER);
         return userRepository.save(user); 
     }
-
+    // Essa função recebe ussername e password, verefica se o username existe, confere se a senha coincide com a senha salva nos arquivos e retorna usuário
+    public User loginUser(String username, String password) {
+    	if(userRepository.existsByUsername(username)) {
+    		User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    		if (passwordEncoder.matches(password, user.getPassword())) {
+    			return user;
+    		}
+    	}
+    	throw new RuntimeException("Usuário não encontrado");
+    }
+    
     public User findByUsername(String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
